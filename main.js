dc1ae5e8ade8320a949f37ffc691cb29328a050b
hotchkiss88 <al-sowa@hotmail.com>
2021-09-01 13:17:53 +0200

    Lepsza wersja - działa

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 function myF(a, b) {
-    if (((a + b) < 0) || ((a - b) < 0) || ((a * b) < 0)) {
+
+    let sum = a + b;
+    let sub = a - b;
+    let multi = a * b;
+
+    if (sum < 0) {
         console.log(
             "Wynik jest nieprawidłowy");
     } else {
-        sum = a + b;
-        sub = a - b;
-        multi = a * b;
-
         console.log("Wynik dodawania wynosi" + " " + sum);
+    }
+
+
+    if (sub < 0) {
+        console.log(
+            "Wynik jest nieprawidłowy");
+    } else {
         console.log("Wynik odejmowania wynosi" + " " + sub);
+    }
+
+
+    if (multi < 0) {
+        console.log(
+            "Wynik jest nieprawidłowy");
+    } else {
         console.log("Wynik mnożenia wynosi" + " " + multi);
     }
 }
 
 
-// myF(15, 2);
+myF(5, 10);
